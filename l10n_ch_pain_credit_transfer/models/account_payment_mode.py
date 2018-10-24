c956ac18123bd1c1fae40e4ff18ee8c571271f65
Timka Piric <timka.piric@braintec-group.com>
2018-10-24 15:34:36 +0200

    t10018 add licence to new files

--- l10n_ch_pain_credit_transfer/models/account_payment_mode.py
+++ l10n_ch_pain_credit_transfer/models/account_payment_mode.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 brain-tec AG (http://www.braintec-group.com)
+# Copyright (c) 2018 brain-tec AG (http://www.braintec-group.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import models, api, _, fields
--- l10n_ch_pain_credit_transfer/models/account_payment_order.py
+++ l10n_ch_pain_credit_transfer/models/account_payment_order.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2018 brain-tec AG (http://www.braintec-group.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 from odoo import models, api, _, fields
 from odoo.exceptions import UserError
 from lxml import etree
