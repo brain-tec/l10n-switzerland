92d8c1aef65109ffb019a7a84f22c541edef0be1
BT-anieto <alberto.nieto@braintec.com>
2024-06-11 16:24:56 +0200

    [MIG] l10n_ch_pain_base: Migration to 16.0
    Remove dependency on l10n_ch_base_bank since it is obsolete

--- l10n_ch_pain_base/models/account_move_line.py
+++ l10n_ch_pain_base/models/account_move_line.py
@@ -14,11 +14,7 @@ class AccountMoveLine(models.Model):
             and self.move_id._has_isr_ref()
             and self.move_id.partner_bank_id
         ):
-            if self.move_id.partner_bank_id._is_qr_iban():
-                vals["communication_type"] = "qrr"
-            else:
-                vals["local_instrument"] = "CH01"
-                vals["communication_type"] = "isr"
+            vals["communication_type"] = "qrr"
             if vals["communication"]:
                 vals["communication"] = vals["communication"].replace(" ", "")
         return vals
